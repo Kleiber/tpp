b265997df936a17e29a8f123eb9ca4f438650e72
Edison Kleiber Ttito <Kleiber100892@gmail.com>
2026-08-18 21:42:32 -0300

    feat: extend debug container support (#20)

--- debug.h
+++ debug.h
@@ -1,17 +1,32 @@
 // author: KleiberXD
 
+#pragma once
+
 #include <iostream>
 #include <queue>
 #include <stack>
 #include <vector>
+#include <tuple>
+#include <string>
 #include <algorithm>
 
 using namespace std;
 
 // Forward declarations for template resolution
-template <typename A> string to_string(A a);
-template <typename A> string to_string(vector<A> a);
+string to_string(bool b);
+string to_string(vector<bool>::reference b);
+string to_string(char c);
+string to_string(string s);
+string to_string(const char* s);
 template <typename A, typename B> string to_string(pair<A, B> p);
+template <typename... A> string to_string(tuple<A...> t);
+template <typename A> string to_string(vector<A> a);
+template <typename A> string to_string(queue<A> a);
+template <typename A> string to_string(stack<A> a);
+template <typename A> string to_string(priority_queue<A> a);
+template <typename A, size_t R> string to_string(A (&a)[R]);
+template <typename A, size_t R, size_t C> string to_string(A (&a)[R][C]);
+template <typename A> string to_string(A a);
 
 #define debug(...) cerr<<"debug:"<<__LINE__<<" "<<#__VA_ARGS__<<": "<<to_string(__VA_ARGS__)<<endl
 #define debugm(...) cerr<<"debug:"<<__LINE__<<" ", debug_out_multiple(#__VA_ARGS__, __VA_ARGS__)
@@ -24,19 +39,23 @@ template <typename A, typename B> string to_string(pair<A, B> p);
         print("debug:%d Time execution %s(): %lld ms\n", __LINE__, f, chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - blockTime.first).count()), blockTime.second = false \
     )
 
-string to_string(bool b) {
+inline string to_string(bool b) {
     return (b?"1":"0");
 }
 
-string to_string(char c){
+inline string to_string(vector<bool>::reference b) {
+    return to_string((bool)b);
+}
+
+inline string to_string(char c){
     return "'" + string({c}) + "'";
 }
 
-string to_string(string s) {
+inline string to_string(string s) {
     return '"' + s + '"';
 }
 
-string to_string(const char* s) {
+inline string to_string(const char* s) {
   return to_string((string) s);
 }
 
@@ -45,14 +64,19 @@ string to_string(pair<A, B> p) {
   return "(" + to_string(p.first) + "," + to_string(p.second) + ")";
 }
 
-template <typename A, typename B, typename C>
-string to_string(tuple<A, B, C> p) {
-  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+template <typename Tuple, size_t... I>
+string tuple_to_string(const Tuple& t, index_sequence<I...>) {
+  string output = "(";
+  bool first = true;
+  (void)initializer_list<int>{
+    (output += (first ? "" : ",") + to_string(get<I>(t)), first = false, 0)...
+  };
+  return output + ")";
 }
 
-template <typename A, typename B, typename C, typename D>
-string to_string(tuple<A, B, C, D> p) {
-  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+template <typename... A>
+string to_string(tuple<A...> t) {
+  return tuple_to_string(t, index_sequence_for<A...>{});
 }
 
 template <typename A>
@@ -78,6 +102,16 @@ string to_string(queue<A> a) {
 
 template <typename A>
 string to_string(stack<A> a) {
+  vector<A> tmp;
+  while (!a.empty()) {
+    tmp.push_back(a.top());
+    a.pop();
+  }
+  return to_string(tmp);
+}
+
+template <typename A>
+string to_string(priority_queue<A> a) {
   vector<A> tmp;
   while (!a.empty()) {
     tmp.push_back(a.top());
@@ -129,14 +163,14 @@ static inline string remove_newlines(const string& s) {
   return result;
 }
 
-void debug_out_multiple(string names) { cerr << endl; }
+inline void debug_out_multiple(string names) { cerr << endl; }
 
 template <typename Head, typename... Tail>
 void debug_out_multiple(string names, Head H, Tail... T) {
   auto pos = names.find(',');
   auto name = names.substr(0, pos);
-  names = names.substr(pos + 1);
-  while(names.front() == ' '){
+  names = (pos == string::npos) ? "" : names.substr(pos + 1);
+  while (!names.empty() && names.front() == ' ') {
     names = names.substr(1);
   }
 
